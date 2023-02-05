010039ba28577a90d4250727fd068c93e0d406ec
msielicki <thirrasch@gmail.com>
2023-02-05 04:05:14 +0100

    no message

--- Engine/Engine/Source/FlavoRootsGame/WaterSystem.cpp
+++ Engine/Engine/Source/FlavoRootsGame/WaterSystem.cpp
@@ -103,28 +103,28 @@ void ft_game::WaterSystem::fixedUpdate(eecs::EntityManager & entities, double de
 IEnumerator ft_game::WaterSystem::drawWaterEffect(CoroutineArg arg) {
 	DrawWaterEffectData* data = static_cast<DrawWaterEffectData*>(arg);
 
-	float timer = 0.0f;
-	while (true) {
-		bool bEnd = timer > maxPlayerTimeInWater;	
-		const float t = timer / maxPlayerTimeInWater;
+	//float timer = 0.0f;
+	//while (true) {
+	//	bool bEnd = timer > maxPlayerTimeInWater;	
+	//	const float t = timer / maxPlayerTimeInWater;
 
-		bool bWindowOpen = true;
-		ImGui::SetNextWindowSize(REL(1.0f, 1.0f), ImGuiCond_Always);
-		ImGui::SetNextWindowPos(REL(0.0f, 0.0f));
-		ImGui::SetNextWindowBgAlpha(0.0f);
-		ImGui::Begin("Water effect window", &bWindowOpen, INVISIBLE());
+	//	bool bWindowOpen = true;
+	//	ImGui::SetNextWindowSize(REL(1.0f, 1.0f), ImGuiCond_Always);
+	//	ImGui::SetNextWindowPos(REL(0.0f, 0.0f));
+	//	ImGui::SetNextWindowBgAlpha(0.0f);
+	//	ImGui::Begin("Water effect window", &bWindowOpen, INVISIBLE());
 
-		ImGui::SetCursorPos(REL((data->bLocalPlayer ? 0.0f : 0.5f), 0.0f));
-		ImGui::Image(IMAGE("ScreenEffects/Rain.png"), REL(0.5f, 1.0f), ImVec4(1.0f, 1.0f, 1.0f, 0.3f + t * 0.7f));
+	//	ImGui::SetCursorPos(REL((data->bLocalPlayer ? 0.0f : 0.5f), 0.0f));
+	//	ImGui::Image(IMAGE("ScreenEffects/Rain.png"), REL(0.5f, 1.0f), ImVec4(1.0f, 1.0f, 1.0f, 0.3f + t * 0.7f));
 
-		ImGui::End();
+	//	ImGui::End();
 
-		if (bEnd)
-			break;
+	//	if (bEnd)
+	//		break;
 
-		timer += framework::FTime::deltaTime;
-		YIELD_RETURN_NULL();
-	}
+	//	timer += framework::FTime::deltaTime;
+	//	YIELD_RETURN_NULL();
+	//}
 
 	EventEntityRespawn* playerResetData = new EventEntityRespawn();
 	playerResetData->entityToRespawn = data->playerEntity;
@@ -132,4 +132,5 @@ IEnumerator ft_game::WaterSystem::drawWaterEffect(CoroutineArg arg) {
 
 	const auto fieldIt = std::find(data->water->playersInWater.begin(), data->water->playersInWater.end(), data->playerEntity);
 	if (fieldIt != data->water->playersInWater.end()) data->water->playersInWater.erase(fieldIt);
+	YIELD_RETURN_NULL();
 }
